f65760834994539915572bdaf8f7690142f311af
Karl Stump <KarlStump@gmail.com>
2022-05-06 10:28:13 -0400

    Here is the initial commit

---
+++ helloworld.c
@@ -0,0 +1,21 @@
+// Simple C program to display "Hello World"
+
+// Header file for input output functions
+#include <stdio.h>
+
+// main function -
+// where the execution of program begins
+int main()
+{
+
+	// prints hello world
+	printf("Hello World!");
+
+	for(int i=0; i < 4; i++){
+	  printf("Hello, I'm %d -- really", i);
+	  
+	}
+
+	return 0;
+}
+
